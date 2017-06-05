b6ab914363d06ab9fe5f3acfc1393130ad7d61d4
tolgahanbayraktar <tolgahanbayraktar@gmail.com>
2017-06-05 17:44:12 +0300

    write player script

---
+++ Assets/Code/CharacterController2D.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController2D : MonoBehaviour {
+
+	public Vector2 Velocity{ get { return _velocity; }}
+
+	Vector2 _velocity;
+
+	public void LateUpdate()
+	{
+		Move (Velocity*Time.deltaTime);
+	}
+
+	private void Move(Vector2 deltaMovement)
+	{
+		transform.Translate (deltaMovement, Space.World);
+	}
+
+	public void SetHorizontalForce(float x)
+	{
+		_velocity.x = x;
+	}
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private CharacterController2D _controller;
+	private float _normalizedHorizontalSpeed;
+
+	void Start()
+	{
+		_controller = GetComponent<CharacterController2D> ();
+	}
+
+	void Update()
+	{
+		HandleInput ();
+	}
+
+	void HandleInput()
+	{
+		if (Input.GetKey (KeyCode.D)) {
+			_normalizedHorizontalSpeed = 1;
+		} else if (Input.GetKey (KeyCode.A)) {
+			_normalizedHorizontalSpeed = -1;
+		} else {
+			_normalizedHorizontalSpeed = 0;
+		}
+
+		_controller.SetHorizontalForce (_normalizedHorizontalSpeed);
+	}
+}
