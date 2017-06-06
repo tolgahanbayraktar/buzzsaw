f94f7f5c1f4725aa2a10cfddd261572994f48a2c
Tolgahan <tolgahanbayraktar@gmail.com>
2017-06-07 01:35:40 +0300

    _raycastBottomLeft calc

--- Assets/Code/CharacterController2D.cs
+++ Assets/Code/CharacterController2D.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour {
 
+	const float SkinWidht=0.02f;
+	const int TotalHorizontalRays = 8;
+	const int TotalVerticalRays = 4;
+
 	public LayerMask PlatformMask;
 	public Vector2 Velocity{ get { return _velocity; }}
-
-	const float SkinWidht=0.02f;
 
 	BoxCollider2D _boxCollider;
 	Vector2 _velocity;
 	Vector3 _localScale;
 	Vector3 _raycastBottomRight;
+	Vector3 _raycastBottomLeft;
+
 	Transform _transform;
+	float _verticalDistanceBetweenRays;
 
 	void Awake()
 	{
 		_boxCollider = GetComponent<BoxCollider2D> ();
 		_localScale = transform.localScale;
 		_transform = transform;
+
+		var ColliderHeight = _boxCollider.size.y * Mathf.Abs (_localScale.y) - (2 * SkinWidht);
+		_verticalDistanceBetweenRays = ColliderHeight / (TotalHorizontalRays - 1);
 	}
 
 	public void LateUpdate()
@@ -40,27 +48,37 @@ public class CharacterController2D : MonoBehaviour {
 		var isGoingRight = deltaMovement.x > 0;
 		var rayDistance = Mathf.Abs (deltaMovement.x) + SkinWidht;
 		var rayDirection = isGoingRight ? Vector2.right : -Vector2.right;
-		var rayOrigin = _raycastBottomRight;
-		var rayVector = rayOrigin;
-		var raycastHit = Physics2D.Raycast (rayVector, rayDirection, rayDistance, PlatformMask);
+		var rayOrigin = isGoingRight ? _raycastBottomRight : _raycastBottomLeft;
 
-		if (!raycastHit)
-			return;
 
-		deltaMovement.x = raycastHit.point.x - rayVector.x;
-		deltaMovement.x -= SkinWidht;
 
-		Debug.DrawRay (rayVector, rayDirection, Color.yellow);
-		Debug.Log (rayDistance);
+		for(var i=0; i < TotalHorizontalRays; i++)
+		{
+			var rayVector = new Vector2 (rayOrigin.x, rayOrigin.y + (i * _verticalDistanceBetweenRays));
+			var raycastHit = Physics2D.Raycast (rayVector, rayDirection, rayDistance, PlatformMask);
+
+			if (!raycastHit)
+				continue;
+
+			deltaMovement.x = raycastHit.point.x - rayVector.x;
+			if (isGoingRight)
+				deltaMovement.x -= SkinWidht;
+			else
+				deltaMovement.x += SkinWidht;
+			
+			Debug.DrawRay (rayVector, rayDirection, Color.yellow);	
+		}
+
 	}
 
 	void CalculateRayOrigins()
 	{
-		var size = new Vector2 (_boxCollider.size.x * Mathf.Abs (_localScale.x), _boxCollider.size.y * Mathf.Abs (_localScale.y));
+		var size = new Vector2 (_boxCollider.size.x * Mathf.Abs (_localScale.x), _boxCollider.size.y * Mathf.Abs (_localScale.y))/2;
 		var center = new Vector2 (_boxCollider.offset.x*_localScale.x, _boxCollider.offset.y*_localScale.y);
 
-		_raycastBottomRight = _transform.position + new Vector3 (center.x + size.x - SkinWidht, center.y - size.y + SkinWidht, 0);
-		//Debug.Log (_raycastBottomRight);
+		_raycastBottomRight = _transform.position + new Vector3 (center.x + size.x - SkinWidht, center.y - size.y + SkinWidht);
+		_raycastBottomLeft = _transform.position + new Vector3 (center.x - size.x + SkinWidht, center.y - size.y + SkinWidht);
+
 	}
 
 	public void SetHorizontalForce(float x)
--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -4,29 +4,44 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	private CharacterController2D _controller;
-	private float _normalizedHorizontalSpeed;
+	public float MaxSpeed = 8;
+
+	CharacterController2D _controller;
+	float _normalizedHorizontalSpeed;
+	bool _isFacingRight;
 
 	void Start()
 	{
 		_controller = GetComponent<CharacterController2D> ();
+		_isFacingRight = transform.localScale.x > 0;
 	}
 
 	void Update()
 	{
 		HandleInput ();
+		_controller.SetHorizontalForce (Mathf.Lerp(_controller.Velocity.x,_normalizedHorizontalSpeed*MaxSpeed, Time.deltaTime));
 	}
 
 	void HandleInput()
 	{
 		if (Input.GetKey (KeyCode.D)) {
 			_normalizedHorizontalSpeed = 1;
+			if (!_isFacingRight)
+				Flip ();
 		} else if (Input.GetKey (KeyCode.A)) {
 			_normalizedHorizontalSpeed = -1;
+			if (_isFacingRight)
+				Flip ();
 		} else {
 			_normalizedHorizontalSpeed = 0;
 		}
-
-		_controller.SetHorizontalForce (_normalizedHorizontalSpeed);
+			
 	}
+
+	void Flip()
+	{
+		transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		_isFacingRight = transform.localScale.x > 0;
+	}
+
 }
