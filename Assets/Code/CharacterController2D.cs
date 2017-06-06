02ff028e1eda1f6f502ed4466c28ce497b210400
tolgahanbayraktar <tolgahanbayraktar@gmail.com>
2017-06-06 17:55:40 +0300

    MoveHorizontally function add

--- Assets/Code/CharacterController2D.cs
+++ Assets/Code/CharacterController2D.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class CharacterController2D : MonoBehaviour {
 
+	public LayerMask PlatformMask;
 	public Vector2 Velocity{ get { return _velocity; }}
 
+	const float SkinWidht=0.02f;
+
+	BoxCollider2D _boxCollider;
 	Vector2 _velocity;
+	Vector3 _localScale;
+	Vector3 _raycastBottomRight;
+	Transform _transform;
+
+	void Awake()
+	{
+		_boxCollider = GetComponent<BoxCollider2D> ();
+		_localScale = transform.localScale;
+		_transform = transform;
+	}
 
 	public void LateUpdate()
 	{
@@ -15,7 +29,38 @@ public class CharacterController2D : MonoBehaviour {
 
 	private void Move(Vector2 deltaMovement)
 	{
-		transform.Translate (deltaMovement, Space.World);
+		CalculateRayOrigins ();
+		MoveHorizontally (ref deltaMovement);
+		_transform.Translate (deltaMovement, Space.World);
+	}
+
+	void MoveHorizontally(ref Vector2 deltaMovement)
+	{
+		
+		var isGoingRight = deltaMovement.x > 0;
+		var rayDistance = Mathf.Abs (deltaMovement.x) + SkinWidht;
+		var rayDirection = isGoingRight ? Vector2.right : -Vector2.right;
+		var rayOrigin = _raycastBottomRight;
+		var rayVector = rayOrigin;
+		var raycastHit = Physics2D.Raycast (rayVector, rayDirection, rayDistance, PlatformMask);
+
+		if (!raycastHit)
+			return;
+
+		deltaMovement.x = raycastHit.point.x - rayVector.x;
+		deltaMovement.x -= SkinWidht;
+
+		Debug.DrawRay (rayVector, rayDirection, Color.yellow);
+		Debug.Log (rayDistance);
+	}
+
+	void CalculateRayOrigins()
+	{
+		var size = new Vector2 (_boxCollider.size.x * Mathf.Abs (_localScale.x), _boxCollider.size.y * Mathf.Abs (_localScale.y));
+		var center = new Vector2 (_boxCollider.offset.x*_localScale.x, _boxCollider.offset.y*_localScale.y);
+
+		_raycastBottomRight = _transform.position + new Vector3 (center.x + size.x - SkinWidht, center.y - size.y + SkinWidht, 0);
+		//Debug.Log (_raycastBottomRight);
 	}
 
 	public void SetHorizontalForce(float x)
