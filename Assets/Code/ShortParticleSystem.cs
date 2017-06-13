7f88d07ac8eb1f5bffa5ebd360254208d65449fb
tolgahanbayraktar <tolgahanbayraktar@gmail.com>
2017-06-13 17:53:59 +0300

    Parallax Background
    Parallax Background

---
+++ Assets/Code/ShortParticleSystem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShortParticleSystem : MonoBehaviour {
+
+	public string LayerName="Particles";
+
+	public void Start()
+	{
+		GetComponent<ParticleSystemRenderer> ().sortingLayerName = LayerName;
+	}
+
+}
