accb47af14ac2c12d1ad9d1d8cf71b759e83c909
tolgahanbayraktar <tolgahanbayraktar@gmail.com>
2017-06-15 17:45:27 +0300

    Pointstar
    checkpoints and star effect add

---
+++ Assets/Code/AutoDestroyParticleSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroyParticleSystem : MonoBehaviour {
+
+	ParticleSystem _particleSystem;
+
+
+	public void Start()
+	{
+		_particleSystem = GetComponent<ParticleSystem> ();
+	}
+
+	public void Update()
+	{
+		// Particle effectini yaptıktan sonra hierarchy kısmından kaybolacak
+		if (_particleSystem.isPlaying)
+			return;
+
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Code/PointStar.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointStar : MonoBehaviour {
+
+	public GameObject Effect;
+	public int PointToAdd = 10;
+
+	public void OnTriggerEnter2D(Collider2D other)
+	{
+		Instantiate (Effect, transform.position, transform.rotation);
+		gameObject.SetActive(false);
+	}
+}
